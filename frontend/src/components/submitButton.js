1422a4e2d7a72020830116f9d3bfb11f467dc727
Rushat Gabhane <rushatgabhane@gmail.com>
2024-08-06 16:57:41 +0300

    show alert dialog using api response

--- frontend/src/components/submitButton.js
+++ frontend/src/components/submitButton.js
@@ -4,6 +4,17 @@ import { useStore } from '../store';
 import { shallow } from 'zustand/shallow';
 import { getGraphDetails } from '../lib/api';
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from './ui/alert-dialog';
+import { useState } from 'react';
+
 const selector = (state) => ({
   nodes: state.nodes,
   edges: state.edges,
@@ -11,18 +22,48 @@ const selector = (state) => ({
 
 function SubmitButton() {
   const { nodes, edges } = useStore(selector, shallow);
+  const [showModal, setShowModal] = useState(false);
+  const [edgeCount, setEdgeCount] = useState(0);
+  const [nodeCount, setNodeCount] = useState(0);
+  const [isAcyclic, setIsAcyclic] = useState(false);
+
   return (
-    <Button
-      className="ml-auto min-w-20"
-      size="sm"
-      onClick={() => {
-        getGraphDetails(nodes, edges).then((data) => {
-          console.log(data);
-        });
-      }}
-    >
-      Submit
-    </Button>
+    <>
+      <Button
+        className="ml-auto min-w-20"
+        size="sm"
+        onClick={() => {
+          getGraphDetails(nodes, edges).then((data) => {
+            setEdgeCount(data.num_edges);
+            setNodeCount(data.num_nodes);
+            setIsAcyclic(data.is_dag);
+            setShowModal(true);
+          });
+        }}
+      >
+        Submit
+      </Button>
+
+      <AlertDialog open={showModal} onOpenChange={setShowModal}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>API response</AlertDialogTitle>
+            <AlertDialogDescription>
+              Number of edges: {edgeCount}
+            </AlertDialogDescription>
+            <AlertDialogDescription>
+              Number of nodes: {nodeCount}
+            </AlertDialogDescription>
+            <AlertDialogDescription>
+              Is the directed graph acyclic: {isAcyclic ? 'Yes' : 'No'}
+            </AlertDialogDescription>
+            <AlertDialogFooter>
+              <AlertDialogAction>Close</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogHeader>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 }
 
